295a91dec87d1758e5a3b86d2d7735d3dc53a61b
hitman <hitman@assassin.brotherhood>
2023-05-14 22:59:34 +0530

    updated the admin path and configured the settings file

--- app/core/settings.py
+++ app/core/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'urls',
 
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,3 +127,7 @@ STATIC_ROOT = '/vol/web/static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://0.0.0.0",
+]
--- app/core/urls.py
+++ app/core/urls.py
@@ -5,7 +5,7 @@ import os
 
 
 urlpatterns = [
-    path(f'admin/{os.environ.get("SECRET_KEY")}/', admin.site.urls),
+    path(f'admin/{os.environ.get("ADMIN_KEY")}/', admin.site.urls),
     path('api/', include('api.urls')),
     re_path(r'(?P<pid>\w+)/$', views.redirect_to_site)
 ]
