6632b75b92a63307cee593bdcd161f6492904382
hitman <hitman@assassin.brotherhood>
2023-05-16 20:34:52 +0530

    setup nginx to allow HTTPS proxying

---
+++ docker/certbot/certify-init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+# Waits for proxy to be available then gets the first certificate
+
+until nc -z proxy 80; do
+    echo "waiting for proxy"
+    sleep 5s & wait ${!}
+done
+
+echo "Getting certificate!"
+
+certbot certonly \
+    --webroot \
+    --webroot-path "/vol/www/" \
+    -d "$DOMAIN" \
+    --email $EMAIL \
+    --rsa-key-size 4096 \
+    --agree-tos \
+    --noninteractive
